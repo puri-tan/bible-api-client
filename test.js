3dae79409e065902f22b1bf94a63fc13dd3730d3
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2022-01-29 16:20:07 -0300

    Adding version selection

---
+++ test.js
@@ -0,0 +1,23 @@
+const BibleApiClient = require('./lib/bible-api-client')
+const dotenv = require('dotenv')
+
+const message = "Joao 1:1-3"
+
+dotenv.config()
+
+const config = {
+  baseURL: process.env.BIBLE_API_URL,
+  token: process.env.BIBLE_API_TOKEN,
+  defaultVersion: process.env.BIBLE_DEFAULT_VERSION
+}
+
+const client = new BibleApiClient(config)
+const groups = client.matchVersesFromMessage(message)
+
+client.pullVersesFromMatch(groups).then(matches => {
+  matches.forEach(match => {
+    match.verses.forEach(verse => {
+      console.log(verse.text)
+    })
+  })
+})
